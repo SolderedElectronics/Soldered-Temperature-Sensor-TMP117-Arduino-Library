833f115b2b7810fcb367563cf8a39265f263a841
Clang Robot <robot@example.com>
2025-09-19 07:18:14 +0000

    Committing clang-format changes

--- src/Soldered-TMP117.h
+++ src/Soldered-TMP117.h
@@ -12,10 +12,10 @@
 
 // Create a custom class constructor that creates a default address
 // if none are provided
-class Soldered_TMP117 : public TMP117 {
-public:
-    Soldered_TMP117(uint8_t address = 0x49) 
-        : TMP117(address) 
+class Soldered_TMP117 : public TMP117
+{
+  public:
+    Soldered_TMP117(uint8_t address = 0x49) : TMP117(address)
     {
     }
 };
